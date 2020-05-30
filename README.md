f6b8785bda0aa0c244e71685618e81d843f2eac2
brajesh <bk04031997@gmail.com>
2020-05-30 11:18:39 +0530

    ArrayBlockingQueue

--- README.md
+++ README.md
@@ -5,3 +5,9 @@
           1. [ArrayList](https://github.com/BrajeshKumarchaudhary/core_java-Data_Structure_Algorithm/blob/master/src/com/java/Collection/MyArrayList.java)
           2. [LinkedList](https://github.com/BrajeshKumarchaudhary/core_java-Data_Structure_Algorithm/blob/master/src/com/java/Collection/MyLinkedlist.java)
           3. [ArrayList Vs LinkedList Vs Vector](https://github.com/BrajeshKumarchaudhary/core_java-Data_Structure_Algorithm/blob/master/src/com/java/Collection/ArrayList_LinkedList_vector.java)
+    2.[Queue]
+          1.[ArrayBlockingQueue]()
+          2.[ConcurrentLinkedQueue]()
+          3.[LinkedBlockingQueue]()
+          4.[LinkedTransferQueue]()
+          5.[PriorityBlockingQueue]()
---
+++ src/com/java/Collection/Queue/ArrayBlockingqueue.java
@@ -0,0 +1,52 @@
+package com.java.Collection.Queue;
+
+import java.util.concurrent.ArrayBlockingQueue;
+
+/*
+ * Here we cover package java.util.Concurrent
+ */
+public class ArrayBlockingqueue {
+  public static void main(String args[])
+  {
+	 /*
+	  * ArrayBlockingQueue
+	  * ArrayBlockingQueue class is a bounded blocking queue backed by an array.
+	  * By bounded it means that the size of the Queue is fixed.
+	  * Once created, the capacity cannot be changed.
+	  * Attempts to put an element into a full queue will result in the operation blocking.
+	  * Similarly attempts to take an element from an empty queue will also be blocked
+	  * Class Heirarchy:Object->AbstarctColection->AbstractQueue->java.util.concurrent.ArrayBlockingQueue
+	  */
+	  /*
+	   * Constructor Summary
+	   * a.ArrayBlockingQueue(int capacity):create an array with given fixed capacity
+	   * b.ArrayBlockingQueue(int capacity,boolean fair):Create an Queue with fixed capacity and fair value true means threads blocked operation like insertion or removal.otherwise unorder
+	   *c.ArrayBlockingQueue(int capacity,boolean fair,Collection c):create Queue with fixed capacity with other collections.
+	   */
+	  
+	  int capacity=20;
+	  //create an ArrayBlockingQueue using capacity
+	  ArrayBlockingQueue<Integer> abq=new ArrayBlockingQueue<>(capacity);
+	  abq.add(20);
+	  abq.add(21);
+	  abq.add(22);
+	  abq.add(23);
+    //print the Queue
+	 abq.forEach(n->System.out.println(n));
+	 //Method Summary
+	 /*
+	  * a.boolean add(E e):insert the element at tail of queue.
+	  * b.void clear():remove the all elements from queue
+	  * c.boolean contains(Object e):return true if specified value contains
+	  * d.int drainTo(Collection c):Remove all the elements from queue and add to another collections
+	  * e.Iteratot iterator():returns iterator over queue in proper sequence 
+	  * f.boolean offer(E e,long timeout,TimeUnit unit):insert the element tail of queue.and wait time to space available.
+	  *g.E peek():return element but not remove head of the queue.
+	  *h.int size():return the size of an array
+	  *i.E take():return and remove the head of this queue.
+	  *j.Object toArray[]:return an array containing all elements in proper sequence 
+	  */
+  }
+	
+	
+}
